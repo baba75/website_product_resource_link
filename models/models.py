d17b69260011f5c5623326ac04a1225d99169ecb
Alberto <baba75@gmail.com>
2019-01-25 08:35:01 -0800

    Added alt description and security

--- models/models.py
+++ models/models.py
@@ -4,9 +4,11 @@ from odoo import models, fields, api
 
 class ProductLinks(models.Model):
     _name = 'product.links'
-
+    _description = 'Product resource link'
+    
     name = fields.Char('Link label', required=True, translate=True)
     url = fields.Char('Link URL', required=True, translate=True)
+    alt_desc = fields.Char('Alt Description', translate=True)
     product_tmpl_id = fields.Many2one('product.template', 'Related Product', copy=True)
 
 class ProductTemplate(models.Model):
